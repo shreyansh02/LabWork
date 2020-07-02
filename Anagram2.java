4210bfc82b01f5226b6fb4da53feb807ad9ae935
Shreyansh Tripathi <42905282+shreyansh02@users.noreply.github.com>
2020-07-02 12:31:16 +0530

    Create Anagram2.java

---
+++ Anagram2.java
@@ -0,0 +1,32 @@
+public class Anagram2 {
+
+	public static void main(String[] args) {
+		
+		String a = "aaeb m";
+		String b = "abaem";
+		
+		boolean isAnagram = true;
+		
+		int al[] = new int[256];
+		
+		for(char c: a.toCharArray()) {  //break the String and each Character is put in Array 
+			 int index =(int) c;
+			 al[index]++;
+		}
+		
+		for(char c: b.toCharArray()) {  //break the String and each Character is put in Array 
+			 int index =(int) c;
+			 al[index]--;
+		}
+		
+		for(int i=0; i<256; i++) {
+			if(al[i] != 0) {
+				isAnagram = false;
+			}
+		}
+		if(isAnagram)
+			System.out.println("Anagram");
+		else
+			System.out.println("Not Anagram");
+	}
+}
