ec6942fe65384d8ecf6ea61c7470a7feb47a2bef
Shreyansh Tripathi <42905282+shreyansh02@users.noreply.github.com>
2020-07-04 08:16:40 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,34 @@
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		String s = "       The sky  is blue ";
+		
+		String ans = "";
+		
+		int i = s.length()-1;
+		
+		while(i >= 0) {
+			
+			while(i >= 0 && s.charAt(i) == ' ') 
+				i--;
+			
+			int j = i;
+			
+			if(i < 0)   // don't print space in last of the string,in-case in starting of string we have space
+				break;
+			
+			while(i >= 0 && s.charAt(i) != ' ')
+				i--;
+			
+			if(ans.isEmpty()) {
+				ans = ans.concat(s.substring(i+1, j+1));
+			}else
+				ans = ans.concat(" "+s.substring(i+1, j+1));
+		}
+		
+		System.out.println(ans);
+		
+	}
+
+}
