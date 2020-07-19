c0b12a30c468abc529373a471eaa2d699e841dbd
Shreyansh Tripathi <42905282+shreyansh02@users.noreply.github.com>
2020-07-19 11:59:14 +0530

    Create Format2.java

---
+++ numberFormatClass/Format2.java
@@ -0,0 +1,12 @@
+//The <b>setParseIntegerOnly()<b> method is a built-in method of the java.text.NumberFormat which sets whether or not numbers should be parsed as integers only.
+
+import java.text.NumberFormat; 
+import java.util.Locale; 
+import java.util.Currency;
+
+public class Format2{
+  public static void main(String[] args){
+      
+    }
+  }
+}
