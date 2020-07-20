9bf3406286e2f25838cc215afb295c2fac139470
Shreyansh Tripathi <42905282+shreyansh02@users.noreply.github.com>
2020-07-20 22:10:03 +0530

    setCurrency() method

--- numberFormatClass/Format3.java
+++ numberFormatClass/Format3.java
@@ -1,4 +1,4 @@
-//The setCurrency() method is a built-in method of the java.text.NumberFormat 
+//The setCurrency() method is a built-in method of the java.text.NumberFormat which sets the currency used by this number format when formating currency values. This does not update the minimum or maximum number of fraction digits usedby the number format. It overwrites the initially currency.
 
 public class Format3{
   
