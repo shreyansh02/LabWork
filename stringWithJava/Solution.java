ba67bddec90634efa4207fe19f3390dbbfa434f7
Shreyansh Tripathi <42905282+shreyansh02@users.noreply.github.com>
2020-07-24 09:27:24 +0530

    Tag Content Extractor

--- stringWithJava/Solution.java
+++ stringWithJava/Solution.java
@@ -1,25 +1,27 @@
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-/* Solution assumes we can't have the symbol "<" as text between tags */
-public class Solution {
-    public static void main(String[] args) {
-        Scanner scan  = new Scanner(System.in);
-        int testCases = Integer.parseInt(scan.nextLine());
-        while (testCases-- > 0) {
-            String line = scan.nextLine();
+public class Solution{
+	public static void main(String[] args){
+		
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases-- >0){
+			String line = in.nextLine();
             boolean matchFound = false;
-            Pattern r = Pattern.compile("<(.+)>([^<]+)</\\1>");
-            Matcher m = r.matcher(line);
-            while (m.find()) {
+            Pattern p = Pattern.compile("<(.+)>([^<]+)</\\1>");
+            Matcher m = p.matcher(line);
+            while(m.find()){
                 System.out.println(m.group(2));
                 matchFound = true;
             }
-            if (!matchFound) {
+            if(!matchFound){
                 System.out.println("None");
             }
-        }
-        scan.close();
-    }
+		}
+        in.close();
+	}
 }
