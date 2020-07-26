80cd08a0d5ad50a7dd0568309ae5c2aac3fad560
Shreyansh Tripathi <42905282+shreyansh02@users.noreply.github.com>
2020-07-26 11:58:32 +0530

    Tag Content Extractor

--- stringWithJava/Regex1.java
+++ stringWithJava/Regex1.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution{
+public class Regex1{
 	public static void main(String[] args){
 		
 		Scanner in = new Scanner(System.in);
