52a5a19038b7e128fc0c33b2b66c5ebb3dcc795b
Shreyansh Tripathi <42905282+shreyansh02@users.noreply.github.com>
2020-07-26 23:17:41 +0530

    Regex Quantifier
    The Quantifier specified the number of occurrence of the character.

---
+++ stringWithJava/Regex4.java
@@ -0,0 +1,14 @@
+import java.util.Regex.*;
+
+public class Regex4{
+  public static void main(String[] args){
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+  }
+}
