723333b496a8aed3152a56305f6fac4b30bbe842
Shreyansh Tripathi <42905282+shreyansh02@users.noreply.github.com>
2020-07-26 12:05:24 +0530

    intro - matching patterns of Regex /

---
+++ stringWithJava/Regex2.java
@@ -0,0 +1,10 @@
+import java.util.Regex.*;
+
+public class Regex2{
+  public static void main(String args[]){
+    Pattern pattern = pattern.compile(".xx.");
+    Matcher matcher = pattern.matcher("AxxB");
+    
+    System.out.println("String matches with given regex: "+ matcher.matches());
+  }
+}
