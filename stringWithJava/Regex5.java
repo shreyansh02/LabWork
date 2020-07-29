fe4c36e0f4e2066fae968f163af6d65dd0d3d6dd
Shreyansh Tripathi <42905282+shreyansh02@users.noreply.github.com>
2020-07-29 21:38:42 +0530

    Update Regex5.java

--- stringWithJava/Regex5.java
+++ stringWithJava/Regex5.java
@@ -2,11 +2,13 @@ import java.util.Regex.*;
 
 public class Regex4{
   public static void main(String[] args){
-    System.out.println("MetaCharacter d ....");
-    System.out.println("", "");
-    System.out.println("", "");
-    System.out.println("", "");
-    System.out.println("", "");
+    
+    System.out.println("MetaCharacter d ...."); // d means digit
+    System.out.println("d", "abc"); // false (non-digits)
+    System.out.println("d", "1"); //
+    System.out.println("d", "4443");
+    System.out.println("d", "321abc");
+    
     System.out.println("", "");
     System.out.println("", "");
     System.out.println("", "");
