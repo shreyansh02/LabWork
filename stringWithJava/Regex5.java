243f483ddad2d5abe741a82474b09c46f98ac12a
Shreyansh Tripathi <42905282+shreyansh02@users.noreply.github.com>
2020-07-29 20:34:07 +0530

    Create Regex5.java

---
+++ stringWithJava/Regex5.java
@@ -0,0 +1,16 @@
+import java.util.Regex.*;
+
+public class Regex4{
+  public static void main(String[] args){
+    System.out.println("MetaCharacter d ....");
+    System.out.println("", "");
+    System.out.println("", "");
+    System.out.println("", "");
+    System.out.println("", "");
+    System.out.println("", "");
+    System.out.println("", "");
+    System.out.println("", "");
+    System.out.println("", "");
+    System.out.println("", "");
+  }
+}
