94a2b205cdba46333dcc75744da5722de5ed70e2
Shreyansh Tripathi <42905282+shreyansh02@users.noreply.github.com>
2020-07-30 09:15:19 +0530

    Pattern Matcher and Email Validation

---
+++ stringWithJava/Demo.java
@@ -0,0 +1,14 @@
+import java.util.Regex.*;
+import java.util.Scanner;
+
+public class Demo{
+  public static void main(String[] args){
+    
+    try(Scanner sc = new Scanner(System.in){
+      while(true){
+        
+      }
+    }
+    
+  }
+}
