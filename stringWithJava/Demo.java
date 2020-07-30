8aea1dbd3e4508f5f95258dc53d9c41afda9284d
Shreyansh Tripathi <42905282+shreyansh02@users.noreply.github.com>
2020-07-30 09:24:04 +0530

    Pattern and Matcher

--- stringWithJava/Demo.java
+++ stringWithJava/Demo.java
@@ -6,7 +6,19 @@ public class Demo{
     
     try(Scanner sc = new Scanner(System.in){
       while(true){
+        System.out.println("Enter the Regex Pattern");
+        String regexPattern = sc.nextLine();
+        Pattern pattern = Pattern.compile(regexPattern);
+        Matcher matcher = pattern.matcher("Welcome to Github");
+        boolean found = false;
         
+        while(matcher.find()){
+          System.out.println("found the text "+ matcher.group() +" strating at index "+ matcher.start() +" and ending at index "+ matcher.end());
+          found = true;
+        }
+        
+        if(!found)
+          System.out.println("match not found");
       }
     }
     
