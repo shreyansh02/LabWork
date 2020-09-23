28c1e151aa0836c3e8a4daad8e7d6dd67ef3a1bb
Shreyansh Tripathi <42905282+shreyansh02@users.noreply.github.com>
2020-09-23 09:40:24 +0530

    Java Substring Comparisons
    Return the respective lexicographically smallest and largest substrings as a single newline-separated string.

--- stringWithJava/stringLexicographic.java
+++ stringWithJava/stringLexicographic.java
@@ -1,3 +1,30 @@
 public class stringLexicographic{
-
+  
+  public static String getSmallestAndLargest(String s, int k){
+    String smallest = "";
+    String largest = "";
+    smallest = largest = s.substring(0, k);
+    
+    for(int i=0; i<=s.length()-k+1; i++){
+      String str = s.substring(i, k+i);
+      
+      if(smallest.compareTo(str) > 0)
+        smallest = str;
+      
+      if(largest.compareTo(str) < 0)
+        largest = str;
+        
+      return smallest +"\n"+ largest
+    }
+  }
+  
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String s = sc.next();
+    int k = sc.nextInt();
+    sc.close();
+    
+    System.out.println(getSmallestAndLargest(s, k));
+  }
+  
 }
